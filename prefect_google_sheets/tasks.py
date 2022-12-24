2992403cd5d935602d8a2084797d29fefada9856
Stefano Cascavilla <cascavilla1@gmail.com>
2022-12-24 17:53:53 +0100

    feat: Defined the signature and first raises for the overwrite_google_sheet_with_df task

--- prefect_google_sheets/tasks.py
+++ prefect_google_sheets/tasks.py
@@ -247,3 +247,53 @@ def read_google_sheet_as_dict_of_lists(
         column_name: sheet_df[column_name].values.tolist()
         for column_name in sheet_df.columns.values
     }
+
+
+@task
+def overwrite_google_sheet_with_df(
+    data_frame: DataFrame = None,
+    is_public_sheet: bool = False,
+    google_service_account: Union[Dict, str] = None,
+    google_sheet_key: Optional[str] = None,
+    google_sheet_name: Optional[str] = None,
+    first_row_header: Optional[bool] = True,
+) -> bool:
+    """
+    This task leverages the Google Sheets API v4 through the gspread library
+    in order to overwrite the content of a sheet with a given pandas dataframe.
+    Args:
+        data_frame: The pandas dataframe to write on the Google Sheet.
+        is_public_sheet: Whether the Google Sheet is public or not.
+            If True, the google_service_account param will be ignored.
+        google_service_account: The Service Account to be used in order to interact with
+            the Google Sheet. This can be a dict or a string representing the JSON
+            Service Account body.
+        google_sheet_key: The key of the Google Sheet to read data from.
+        google_sheet_name: The name of the Sheet to read data from.
+        first_row_header: Whether the first row is the header.
+            If True, the first row will be used as header and data won't be read.
+            Otherwise, if set to False. Default set to True
+    Raises:
+        - `GoogleSheetsConfigurationException`
+            if google_service_account not provided or None
+        - `GoogleSheetsConfigurationException`
+            if google_sheet_key not provided or None
+        - `GoogleSheetsConfigurationException`
+            if google_sheet_name not provided or None
+        - `GoogleSheetServiceAccountError`
+            if the google_service_account is not valid or malformed
+    Returns:
+        True or False, depending if the write operation went well.
+    """
+
+    if not is_public_sheet and not google_service_account:
+        exc_message = "Missing Google Service Account information."
+        raise GoogleSheetsConfigurationException(exc_message)
+
+    if not google_sheet_key:
+        exc_message = "Missing the Google Sheet key identifier."
+        raise GoogleSheetsConfigurationException(exc_message)
+
+    if not google_sheet_name:
+        exc_message = "Missing the Google Sheet name identifier."
+        raise GoogleSheetsConfigurationException(exc_message)
