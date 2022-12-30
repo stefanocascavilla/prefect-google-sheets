c85eae589fe83c32b05320af24ddc4aeb3c8e0e5
Stefano Cascavilla <cascavilla1@gmail.com>
2022-12-30 10:56:12 +0100

    feat: Added new exceptions and changed the base class

--- prefect_google_sheets/exceptions.py
+++ prefect_google_sheets/exceptions.py
@@ -2,20 +2,34 @@
 Exceptions to be used when working with Google Sheets
 """
 
+from gspread.exceptions import GSpreadException
 
-class GoogleSheetsConfigurationException(Exception):
+
+class GoogleSheetsConfigurationException(GSpreadException):
     """
     An exception happened if Google parameters are omitted or wrong.
     """
 
 
-class GoogleSheetValueError(Exception):
+class GoogleSheetValueError(GSpreadException):
     """
     An exception happened if an error is thrown while working with the sheet.
     """
 
 
-class GoogleSheetServiceAccountError(Exception):
+class GoogleSheetServiceAccountError(GSpreadException):
     """
     An exception happened if an error is thrown while using the service account.
+    """
+
+
+class GoogleSheetClearException(GSpreadException):
+    """
+    An exception happened while trying to clear a Google Sheet.
+    """
+
+
+class GoogleSheetWriteException(GSpreadException):
+    """
+    An Exception happened while trying to write on a Google Sheet.
     """
